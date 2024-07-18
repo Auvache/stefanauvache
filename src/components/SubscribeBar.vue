e240174e87ebb88357f0a13980775a0f6381de94
Stefan Bradley <sbradley@taxhawk.com>
2024-07-18 14:03:11 -0700

    init 2

--- src/components/SubscribeBar.vue
+++ src/components/SubscribeBar.vue
@@ -2,15 +2,16 @@
   <section class="py-3 bg-dark-blue">
     <div class="container medium-content text-center">
       <div>
-        <p class="white">Subscribe to my <span class="fw-bold fst-italic">Food 4 Thought</span> news letter for recommendations and lessons from technology, music, books, and life.</p>
+        <p style="margin-bottom: -1.75rem;" class="white">Subscribe to my <span class="fw-bold fst-italic">Food 4 Thought</span> news letter for recommendations and lessons from technology, music, books, and life.</p>
         <div>
-          <form class="subscribe d-flex flex-column flex-sm-row justify-content-center align-items-center" @submit.prevent="">
-            <label for="email" class="sr-only">Subscribe</label>
-            <input id="email" name="email" type="text" class="me-0 mb-2 me-sm-2 mb-sm-0" placeholder="Email Address" />
-            <input type="submit" class="btn btn-white" value="Subscribe">
-          </form>
+          <div class="subscribe d-flex flex-column flex-sm-row justify-content-center align-items-center">
+            <component is="script" async data-uid="0396ab84d5" src="https://stefan-bradley.ck.page/0396ab84d5/index.js"></component>
+          </div>
+
         </div>
       </div>
     </div>
   </section>
 </template>
+<script setup>
+</script>
