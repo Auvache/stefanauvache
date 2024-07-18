2c5a8cbf3275b795d3868823993d2fcaa9069d63
Stefan Bradley <sbradley@taxhawk.com>
2024-07-18 14:10:46 -0700

    init 2

--- src/components/SubscribeBar.vue
+++ src/components/SubscribeBar.vue
@@ -2,7 +2,7 @@
   <section class="py-3 bg-dark-blue">
     <div class="container medium-content text-center">
       <div>
-        <p style="margin-bottom: -1.75rem;" class="white">Subscribe to my <span class="fw-bold fst-italic">Food 4 Thought</span> news letter for recommendations and lessons from technology, music, books, and life.</p>
+        <p class="white mb-0">Subscribe to my <span class="fw-bold fst-italic">Food 4 Thought</span> news letter for recommendations and lessons from technology, music, books, and life.</p>
         <div>
           <div class="subscribe d-flex flex-column flex-sm-row justify-content-center align-items-center">
             <component is="script" async data-uid="0396ab84d5" src="https://stefan-bradley.ck.page/0396ab84d5/index.js"></component>
