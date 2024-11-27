8cb81639e7b063e6404fade616515897e5b358ee
Stefan Bradley <sbradley@taxhawk.com>
2024-11-27 09:06:05 -0800

    update music page

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -3,10 +3,12 @@
     <div class="container thinnest-content">
       <div>
         <h1 class="text-sm-center">Music</h1>
-        <p>You may be wondering why music is included on a site about practical lessons found in the world of technology.</p>
         <p>Simply stated, I have learned to spend my time doing what I love. I have played the guitar since I was 11 years old and have no intention of stopping anytime soon.</p>
-        <p>I have no other agenda here than to share music that I love and music that I create.</p>
-        <p>More coming soon...</p>
+        <p>I have no agenda here than to share music that I love and music that I create.</p>
+
+        <h2>My music</h2>
+        <h3>Acoustic demos</h3>
+        <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1836998508&color=%23407aaa&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;"><a href="https://soundcloud.com/stefan-bradley-935634534" title="Havre De Grace" target="_blank" style="color: #cccccc; text-decoration: none;">Havre De Grace</a> · <a href="https://soundcloud.com/stefan-bradley-935634534/sets/demos" title="Demos" target="_blank" style="color: #cccccc; text-decoration: none;">Demos</a></div>
       </div>
     </div>
   </section>
